a2d38aca35f13142c9f7d5b8925bca82105865bc
Play9win <rohit.kumar@sourcefuse.com>
2019-12-02 01:46:22 +0530

    added env vars

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    ggApiKey: process.env.GG_API_KEY || ENV['ggapi'],
-    ggCx: process.env.GG_CX || ENV['ggcx'],
-    discordToken: process.env.DT || ENV['dt']
+    ggApiKey: process.env.GG_API_KEY || '',
+    ggCx: process.env.GG_CX || '',
+    discordToken: process.env.DT || ''
   }
