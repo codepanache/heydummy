1f79557f0e19aeb982a6046dba8a94ef7090b956
Play9win <rohit.kumar@sourcefuse.com>
2019-12-02 02:16:14 +0530

    added env logic

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    ggApiKey: process.env.GG_API_KEY || '',
-    ggCx: process.env.GG_CX || '',
-    discordToken: process.env.DT || ''
+    ggApiKey: process.env.ggapi || '',
+    ggCx: process.env.ggcx || '',
+    discordToken: process.env.dt || ''
   }
